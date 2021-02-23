81958ed1617586d2fe06adcc1afdce1697881019
Hala Alkhellow <halakhellow1996@gmail.com>
2021-02-23 19:25:40 +0200

    refactor Hangman component and use new images into it + fix the style for it

--- src/components/Hangman/Hangman.js
+++ src/components/Hangman/Hangman.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 import AlphaButtons from "../AlphaButtons/AlphaButtons";
 
-import img0 from "../../images/0.jpg";
-import img1 from "../../images/1.jpg";
-import img2 from "../../images/2.jpg";
-import img3 from "../../images/3.jpg";
-import img4 from "../../images/4.jpg";
-import img5 from "../../images/5.jpg";
-import img6 from "../../images/6.jpg";
+import img0 from "../../images/0.png";
+import img1 from "../../images/1.png";
+import img2 from "../../images/2.png";
+import img3 from "../../images/3.png";
+import img4 from "../../images/4.png";
+import img5 from "../../images/5.png";
+import img6 from "../../images/6.png";
 
 import { randomWord } from "./words";
 
@@ -23,23 +23,22 @@ class Hangman extends Component {
   constructor(props) {
     super(props);
     this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
-    this.handleGuess = this.handleGuess.bind(this);
-    this.reset = this.reset.bind(this);
   }
 
   guessedWord() {
     return this.state.answer
       .split("")
-      .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"));
+      .map((letter) => (this.state.guessed.has(letter) ? letter : "_"));
   }
 
-  handleGuess(evt) {
-    let ltr = evt.target.value;
+  handleGuess = (evt) => {
+    let letter = evt.target.value;
     this.setState((st) => ({
-      guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+      guessed: st.guessed.add(letter),
+      nWrong: st.nWrong + (st.answer.includes(letter) ? 0 : 1),
     }));
-  }
+  };
+
   generateButtons() {
     return (
       <AlphaButtons
@@ -49,38 +48,40 @@ class Hangman extends Component {
       />
     );
   }
-  reset() {
+
+  reset = () => {
     this.setState({
       nWrong: 0,
       guessed: new Set(),
       answer: randomWord(),
     });
-  }
+  };
+
   render() {
+    let { nWrong, answer } = this.state;
+    let { maxWrong, images } = this.props;
     let gameState = this.generateButtons();
-    if (this.state.nWrong === this.props.maxWrong) gameState = "GAME OVER !";
-    if (this.guessedWord().join("") === this.state.answer)
-      gameState = "You Win!";
+    if (nWrong === maxWrong) gameState = "GAME OVER !";
+    if (this.guessedWord().join("") === answer) gameState = "CORRECT !";
+    let gameEnded =
+      nWrong === maxWrong || this.guessedWord().join("") === answer;
     return (
       <div className="Hangman">
         <h1>Hangman</h1>
         <div className="img-container">
-          <img
-            src={this.props.images[this.state.nWrong]}
-            alt={`${this.state.nWrong} wrong guesses`}
-          />
+          <img src={images[nWrong]} alt={`${nWrong} wrong guesses`} />
         </div>
         <div className="Hangman-content">
-          <p>Attempts left : {this.props.maxWrong - this.state.nWrong}</p>
-          <p className="Hangman-word">
-            {this.state.nWrong === this.props.maxWrong
-              ? this.state.answer
-              : this.guessedWord()}
+          <p>Attempts left :{` ${maxWrong - nWrong} / ${maxWrong}`}</p>
+          <p className={`Hangman-word ${gameEnded && "big-size"}`}>
+            {nWrong === maxWrong ? answer : this.guessedWord()}
           </p>
           <div className="Hangman-status">{gameState}</div>
-          <button onClick={this.reset} className="Hangman-reset">
-            Reset
-          </button>
+          {gameEnded && (
+            <button onClick={this.reset} className="Hangman-reset">
+              Next word
+            </button>
+          )}
         </div>
       </div>
     );
