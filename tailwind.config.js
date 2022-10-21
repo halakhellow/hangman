aabd8bebbbb045b9a7f78cc1ec8b8fa9b83faf5e
Hala Alkhellow <halakhellow1996@gmail.com>
2022-10-21 18:47:34 +0300

    add custom configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkYellow: "#fce728",
+        maroon: "#800000",
+        lightGreen: "#006400ab",
+        darkBlack: "#080808",
+        whitesmoke: "#f5f5f5",
+        boardGreen: "#177b3d",
+      },
+    },
   },
   plugins: [],
 };
